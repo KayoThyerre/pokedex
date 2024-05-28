d6f1848287cb168da693b0dc0b12265350755ed6
Kayo Thyerre <kayothyerrework@gmail.com>
2024-05-28 17:34:09 -0300

    update app.jsx + api.jsx | add pokemon.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { gethPokemonData, gethPokemons } from './components/api.jsx';
 import Navbar from './components/navbar.jsx';
+import Pokedex from './components/pokedex.jsx';
 import Searchbar from './components/serchbar.jsx';
 
+
 const App = () => {
+  
+  const [loading, setLoading] = useState(false);
+  const [pokemons, setPokemons] = useState([]);
+
+  const fetchPokemons = async () => {
+    try {
+      setLoading(true)
+      const data = await gethPokemons();
+      const promises = data.results.map(async (pokemon) => {
+        return await gethPokemonData(pokemon.url);
+      });
+
+      const results = await Promise.all(promises);
+      setPokemons(results);
+      setLoading(false);
+    } catch (error) {
+      console.log("fetchPokemons error: ", error);
+    }
+  }
+
+  useEffect(() => {
+    console.log("carregou")
+    fetchPokemons();
+  }, [])
+  
   return (
     <>
       <Navbar />
       <Searchbar />
+      <Pokedex pokemons={pokemons} loading={loading}/>
     </>
   );
 }
